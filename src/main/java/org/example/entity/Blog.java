cb356d5bd64409d235aeb4bf15a65de72333f01a
John <3133549614@qq.com>
2024-04-09 22:23:20 +0800

    two commits five entity files

--- src/main/java/org/example/entity/Blog.java
+++ src/main/java/org/example/entity/Blog.java
@@ -1,5 +1,61 @@
 package org.example.entity;
 
-public class Blog {
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+
+@Data//automatic generation get or set
+@EqualsAndHashCode(callSuper = false)//automatic generation equals or hashcode
+@Accessors(chain = true) //
+@TableName("tb-blog")//linked database table
+public class Blog implements Serializable {
+
+       private static final long serialVersionUID = 1L;
+
+       @TableId(value = "id", type = IdType.AUTO)
+       private  Long  id;
+
+       private  Long shopid;
+
+       private  Long userid;
+
+       @TableField(exist = false)
+       private  String icon;
+
+       @TableField(exist = false)
+       private  String name;
+
+       @TableField(exist = false)
+       private  Boolean islike;
+
+       private  String title;
+
+       private  String images;
+
+       private  String content;
+
+       private  Integer liked;
+
+       private  Integer comments;
+
+       private LocalDateTime createTime;
+
+       private LocalDateTime updateTime;
+
+
+
+
+
+
+
+
 
 }
---
+++ src/main/java/org/example/entity/SeckillVoucher.java
@@ -0,0 +1,35 @@
+package org.example.entity;
+
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("tb_seckill_voucher")
+public class SeckillVoucher implements Serializable {
+
+    private static final  long SerialVersion = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long voucherId;
+
+    private Integer stock;
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime beginTime;
+
+    private LocalDateTime updateTime;
+
+
+
+}
